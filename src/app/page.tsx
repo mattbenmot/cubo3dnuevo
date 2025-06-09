1337dd5d127be939ddc4c6c90f5b3f0b2eaa7d82
mattbenmot <gre.gfredderick@gmail.com>
2025-06-09 14:17:24 -0300

    cambio de color bg a ambar-50

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ import CuboConImagen from "@/components/CuboConImagen";
 export default function Home() {
   return (
     <div className="w-full h-[300vh] bg-[#131466]" >
-      <div className="w-full h-[100vh] bg-[#240d6999] " >
+      <div className="w-full h-[100vh] bg-amber-50 " >
         {/* <Text3DHover /> */}
         <CuboConImagen />
       </div>
